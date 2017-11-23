04051a620154640c7415205c556cf2dabaaa3832
Lorik Dumani <s4loduma@uni-trier.de>
2017-11-23 16:36:56 +0100

    * Added script to aggregate csv files "per version". * Added script to find posts that have false positives although they use equal methods.

--- metric_comparison_r_scripts/metric_comparison.R
+++ metric_comparison_r_scripts/metric_comparison.R
@@ -1,36 +1,77 @@
 # TODO set relative path
-setwd("C:\\Users\\Lorik\\Documents\\GitHub\\r-scripts\\metric_comparison_r_scripts\\2017-11-14_sample_comparison_sebastian-4") # Pfad bitte anpassen
+setwd("C:\\Users\\Lorik\\Desktop\\5. Semester\\Master-Arbeit\\2017-11-22_sample_comparison_sebastian-5") # Pfad bitte anpassen
 
 
 ############################################################################################
 # parse all files "..._per_post.csv" and create an aggregated one "PostId_VersionCount_SO_17-06_sample_100_aggregated.csv"
 
 library(data.table)
-data_1 <- fread("PostId_VersionCount_SO_17-06_sample_100_1_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
-data_1plus <- fread("PostId_VersionCount_SO_17-06_sample_100_1+_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
-data_2 <- fread("PostId_VersionCount_SO_17-06_sample_100_2_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
-data_2plus <- fread("PostId_VersionCount_SO_17-06_sample_100_2+_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
-data_multiple_possible_links <- fread("PostId_VersionCount_SO_17-06_sample_100_multiple_possible_links_per_post.csv", header=TRUE, sep=";", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
-data_unclear_matching <- fread("PostId_VersionCount_SO_17_06_sample_unclear_matching_per_post.csv", header=TRUE, sep=";", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
-data_java_1 <- fread("PostId_VersionCount_SO_Java_17-06_sample_100_1_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
-data_java_2 <- fread("PostId_VersionCount_SO_Java_17-06_sample_100_2_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+
+data_1_perPost <- fread("PostId_VersionCount_SO_17-06_sample_100_1_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_1plus_perPost <- fread("PostId_VersionCount_SO_17-06_sample_100_1+_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_2_perPost <- fread("PostId_VersionCount_SO_17-06_sample_100_2_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_2plus_perPost <- fread("PostId_VersionCount_SO_17-06_sample_100_2+_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_multiple_possible_links_perPost <- fread("PostId_VersionCount_SO_17-06_sample_100_multiple_possible_links_per_post.csv", header=TRUE, sep=";", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_unclear_matching_perPost <- fread("PostId_VersionCount_SO_17_06_sample_unclear_matching_per_post.csv", header=TRUE, sep=";", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_java_1_perPost <- fread("PostId_VersionCount_SO_Java_17-06_sample_100_1_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_java_2_perPost <- fread("PostId_VersionCount_SO_Java_17-06_sample_100_2_per_post.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
 
 # https://stackoverflow.com/a/23933873
-data <- rbindlist(list(data_1, data_1plus), use.names=TRUE, fill=TRUE)
-data <- rbindlist(list(data, data_2), use.names=TRUE, fill=TRUE)
-data <- rbindlist(list(data, data_2plus), use.names=TRUE, fill=TRUE)
-data <- rbindlist(list(data, data_multiple_possible_links), use.names=TRUE, fill=TRUE)
-data <- rbindlist(list(data, data_unclear_matching), use.names=TRUE, fill=TRUE)
-data <- rbindlist(list(data, data_java_1), use.names=TRUE, fill=TRUE)
-data <- rbindlist(list(data, data_java_2), use.names=TRUE, fill=TRUE)
+data_perPost <- rbindlist(list(data_1_perPost, data_1plus_perPost), use.names=TRUE, fill=TRUE)
+data_perPost <- rbindlist(list(data_perPost, data_2_perPost), use.names=TRUE, fill=TRUE)
+data_perPost <- rbindlist(list(data_perPost, data_2plus_perPost), use.names=TRUE, fill=TRUE)
+data_perPost <- rbindlist(list(data_perPost, data_multiple_possible_links_perPost), use.names=TRUE, fill=TRUE)
+data_perPost <- rbindlist(list(data_perPost, data_unclear_matching_perPost), use.names=TRUE, fill=TRUE)
+data_perPost <- rbindlist(list(data_perPost, data_java_1_perPost), use.names=TRUE, fill=TRUE)
+data_perPost <- rbindlist(list(data_perPost, data_java_2_perPost), use.names=TRUE, fill=TRUE)
 
-names(data) <- c("Sample", "Metric", "Threshold", "PostId", "PostVersionCount", "PostBlockVersionCount", "PossibleConnections", "RuntimeTextTotal", "RuntimeTextCPU", "RuntimeTextUser", "TextBlockVersionCount", "PossibleConnectionsText", "TruePositivesText", "FalsePositivesText", "TrueNegativesText", "FalseNegativesText", "RuntimeCodeTotal", "RuntimeCodeCPU", "RuntimeCodeUser", "CodeBlockVersionCount", "PossibleConnectionsCode", "TruePositivesCode", "FalsePositivesCode", "TrueNegativesCode", "FalseNegativesCode")
+names(data_perPost) <- c("Sample", "MetricType", "Metric", "Threshold", "PostId", "PostVersionCount", "PostBlockVersionCount", "PossibleConnections", "RuntimeText", "TextBlockVersionCount", "PossibleConnectionsText", "TruePositivesText", "TrueNegativesText", "FalsePositivesText", "FalseNegativesText", "FailedPredecessorComparisonsText", "RuntimeCode", "CodeBlockVersionCount", "PossibleConnectionsCode", "TruePositivesCode", "TrueNegativesCode", "FalsePositivesCode", "FalseNegativesCode", "FailedPredecessorComparisonsCode")
 
 # https://stackoverflow.com/a/10608587
-# write.table(data, "PostId_VersionCount_SO_17-06_sample_100_aggregated.csv", sep=";", row.names = F)
+write.table(data_perPost, "PostId_VersionCount_SO_17-06_sample_100_aggregated_perPost.csv", sep=";", row.names = F)
+
+
+
+
+
+
+data_1_perVersion <- fread("PostId_VersionCount_SO_17-06_sample_100_1_per_version.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_1plus_perVersion <- fread("PostId_VersionCount_SO_17-06_sample_100_1+_per_version.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_2_perVersion <- fread("PostId_VersionCount_SO_17-06_sample_100_2_per_version.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_2plus_perVersion <- fread("PostId_VersionCount_SO_17-06_sample_100_2+_per_version.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_multiple_possible_links_perVersion <- fread("PostId_VersionCount_SO_17-06_sample_100_multiple_possible_links_per_version.csv", header=TRUE, sep=";", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_unclear_matching_perVersion <- fread("PostId_VersionCount_SO_17_06_sample_unclear_matching_per_version.csv", header=TRUE, sep=";", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_java_1_perVersion <- fread("PostId_VersionCount_SO_Java_17-06_sample_100_1_per_version.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+data_java_2_perVersion <- fread("PostId_VersionCount_SO_Java_17-06_sample_100_2_per_version.csv", header=TRUE, sep=";", quote="", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"))
+
+# https://stackoverflow.com/a/23933873
+data_perVersion <- rbindlist(list(data_1_perVersion, data_1plus_perVersion), use.names=TRUE, fill=TRUE)
+data_perVersion <- rbindlist(list(data_perVersion, data_2_perVersion), use.names=TRUE, fill=TRUE)
+data_perVersion <- rbindlist(list(data_perVersion, data_2plus_perVersion), use.names=TRUE, fill=TRUE)
+data_perVersion <- rbindlist(list(data_perVersion, data_multiple_possible_links_perVersion), use.names=TRUE, fill=TRUE)
+data_perVersion <- rbindlist(list(data_perVersion, data_unclear_matching_perVersion), use.names=TRUE, fill=TRUE)
+data_perVersion <- rbindlist(list(data_perVersion, data_java_1_perVersion), use.names=TRUE, fill=TRUE)
+data_perVersion <- rbindlist(list(data_perVersion, data_java_2_perVersion), use.names=TRUE, fill=TRUE)
+
+names(data_perVersion) <- c("Sample", "MetricType", "Metric", "Threshold", "PostId", "PostHistoryId", "PossibleConnections", "RuntimeText", "TextBlockCount", "PossibleConnectionsText", "TruePositivesText", "TrueNegativesText", "FalsePositivesText", "FalseNegativesText", "FailedPredecessorComparisonsText", "RuntimeCode", "CodeBlockCount", "PossibleConnectionsCode", "TruePositivesCode", "TrueNegativesCode", "FalsePositivesCode", "FalseNegativesCode", "FailedPredecessorComparisonsCode")
+
+# https://stackoverflow.com/a/10608587
+write.table(data_perVersion, "PostId_VersionCount_SO_17-06_sample_100_aggregated_perVersion.csv", sep=";", row.names = F)
+
+
 ############################################################################################
 
 
+
+# Posts that have false positives in TEXT although they use a equals method.
+unique(data_perPost$PostId[
+  (data_perPost$Metric == "equals" | data_perPost$Metric == "equalsNormalized" | data_perPost$Metric == "tokenEquals" | data_perPost$Metric == "tokenEqualsNormalized")
+  & data_perPost$FalsePositivesText != 0])
+
+# Posts that have false positives in CODE although they use a equals method.
+  unique(data_perPost$PostId[
+    (data_perPost$Metric == "equals" | data_perPost$Metric == "equalsNormalized" | data_perPost$Metric == "tokenEquals" | data_perPost$Metric == "tokenEqualsNormalized")
+    & data_perPost$FalsePositivesCode != 0])
 
 
 ############################################################################################
