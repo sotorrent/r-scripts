e6cae4b393d3d3e96c69a742d45e23db29296a5e
Sebastian Baltes <research@sbaltes.com>
2017-12-05 23:42:22 +0100

    Update metrics selection to most recent results

--- metric-selection/metric-selection.R
+++ metric-selection/metric-selection.R
@@ -4,7 +4,7 @@ library(data.table)
 metric_comparison <- fread("MetricComparison_aggregated.csv", header=TRUE, sep=";", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"), stringsAsFactors=FALSE)
 
 nrow(metric_comparison)
-# 938
+# 1474
 
 
 ### TEXT ###
@@ -13,24 +13,29 @@ MatthewsCorrelationText <- metric_comparison$MatthewsCorrelationText
 
 summary(MatthewsCorrelationText)
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 0.7718  0.9240  0.9555  0.9380  0.9670  0.9776 
+# 0.7723  0.9224  0.9633  0.9338  0.9711  0.9793 
 
 boxplot(MatthewsCorrelationText)
 
 MatthewsCorrelationText_99 <- MatthewsCorrelationText[MatthewsCorrelationText >= quantile(MatthewsCorrelationText, 0.99)]
 
 length(MatthewsCorrelationText_99)
-# 10
+# 15
 
 unique(metric_comparison[metric_comparison$MatthewsCorrelationText %in% MatthewsCorrelationText_99,]$Metric)
-# [1] "cosineFourGramNormalizedBool"                    
-# [2] "cosineFourGramNormalizedNormalizedTermFrequency" 
-# [3] "cosineTokenNormalizedBool"                       
-# [4] "cosineThreeGramNormalizedNormalizedTermFrequency"
-# [5] "cosineThreeGramNormalizedBool"                   
-# [6] "cosineTwoShingleNormalizedBool"                  
-# [7] "cosineFiveGramNormalizedBool"                    
-# [8] "cosineTokenNormalizedNormalizedTermFrequency"   
+# [1] "manhattanThreeGramNormalized"                     
+# [2] "fourGramDice"                                     
+# [3] "cosineTwoShingleNormalizedTermFrequency"          
+# [4] "cosineTwoShingleNormalizedNormalizedTermFrequency"
+# [5] "cosineFourGramNormalizedBool"                     
+# [6] "cosineThreeGramNormalizedNormalizedTermFrequency" 
+# [7] "tokenDiceNormalized"                              
+# [8] "cosineFiveGramNormalizedBool"                     
+# [9] "manhattanFourGramNormalized"                      
+# [10] "cosineThreeGramNormalizedBool"                    
+# [11] "cosineFiveGramNormalizedNormalizedTermFrequency"  
+# [12] "manhattanFiveGramNormalized"                      
+# [13] "cosineFourGramNormalizedNormalizedTermFrequency"  
 
 
 ### CODE ###
@@ -39,28 +44,56 @@ MatthewsCorrelationCode <- metric_comparison$MatthewsCorrelationCode
 
 summary(MatthewsCorrelationCode)
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 0.8582  0.9577  0.9712  0.9625  0.9781  0.9830
+# 0.8586  0.9568  0.9733  0.9587  0.9795  0.9846 
 
 boxplot(MatthewsCorrelationCode)
 
 MatthewsCorrelationCode_99 <- MatthewsCorrelationCode[MatthewsCorrelationCode >= quantile(MatthewsCorrelationCode, 0.99)]
 
 length(MatthewsCorrelationCode_99)
-# 10
+# 16
 
 unique(metric_comparison[metric_comparison$MatthewsCorrelationCode %in% MatthewsCorrelationCode_99,]$Metric)
-# [1] "cosineFiveGramNormalizedNormalizedTermFrequency"
-# [2] "winnowingFourGramDice"                          
-# [3] "cosineFourGramNormalizedBool"                   
-# [4] "manhattanTokenNormalized"                       
-# [5] "fourGramDiceNormalizedPadding"                  
-# [6] "winnowingFiveGramDice"                          
-# [7] "fiveGramDiceNormalizedPadding"                  
-# [8] "cosineTokenNormalizedBool"                      
-# [9] "winnowingThreeGramDice"                         
-# [10] "cosineTokenNormalizedNormalizedTermFrequency"  
+# [1] "fourGramJaccardNormalizedPadding" "fiveGramDiceNormalizedPadding"   
+# [3] "fourGramDice"                     "cosineFourGramNormalizedBool"    
+# [5] "cosineThreeGramNormalizedBool"    "fiveGramJaccard"                 
+# [7] "winnowingFiveGramDice"            "manhattanTokenNormalized"        
+# [9] "fourGramDiceNormalizedPadding"    "winnowingFourGramDice"           
+# [11] "cosineFiveGramNormalizedBool"     "winnowingFiveGramJaccard"        
+# [13] "fiveGramDiceNormalized"           "fourGramDiceNormalized"          
+# [15] "winnowingFourGramJaccard"    
 
 
 ### BOTH ###
 
 boxplot(MatthewsCorrelationText, MatthewsCorrelationCode)
+
+unique(c(
+  metric_comparison[metric_comparison$MatthewsCorrelationText %in% MatthewsCorrelationText_99,]$Metric,
+  metric_comparison[metric_comparison$MatthewsCorrelationCode %in% MatthewsCorrelationCode_99,]$Metric
+))
+# [1] "manhattanThreeGramNormalized"                     
+# [2] "fourGramDice"                                     
+# [3] "cosineTwoShingleNormalizedTermFrequency"          
+# [4] "cosineTwoShingleNormalizedNormalizedTermFrequency"
+# [5] "cosineFourGramNormalizedBool"                     
+# [6] "cosineThreeGramNormalizedNormalizedTermFrequency" 
+# [7] "tokenDiceNormalized"                              
+# [8] "cosineFiveGramNormalizedBool"                     
+# [9] "manhattanFourGramNormalized"                      
+# [10] "cosineThreeGramNormalizedBool"                    
+# [11] "cosineFiveGramNormalizedNormalizedTermFrequency"  
+# [12] "manhattanFiveGramNormalized"                      
+# [13] "cosineFourGramNormalizedNormalizedTermFrequency"  
+# [14] "fourGramJaccardNormalizedPadding"                 
+# [15] "fiveGramDiceNormalizedPadding"                    
+# [16] "fiveGramJaccard"                                  
+# [17] "winnowingFiveGramDice"                            
+# [18] "manhattanTokenNormalized"                         
+# [19] "fourGramDiceNormalizedPadding"                    
+# [20] "winnowingFourGramDice"                            
+# [21] "winnowingFiveGramJaccard"                         
+# [22] "fiveGramDiceNormalized"                           
+# [23] "fourGramDiceNormalized"                           
+# [24] "winnowingFourGramJaccard" 
+
