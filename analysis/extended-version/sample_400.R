b180b67b5e5cc43850bf5cc880816e6d50f8e1f0
Sebastian Baltes <research@sbaltes.com>
2019-04-09 14:03:46 +0200

    Refactor script, also remove HTML anchor links

--- analysis/extended-version/sample_400.R
+++ analysis/extended-version/sample_400.R
@@ -18,6 +18,20 @@ if (dir.exists(output_dir)) {
 }
 dir.create(output_dir)
 
+remove_links_and_parse <- function(markdown_content) {
+  markdown_content <- str_replace_all(markdown_content, "&#xD;&#xA;", "\n") # replace escaped newlines
+  markdown_content <- str_replace_all(markdown_content, "\"\"", "\"") # replace escaped double quotes
+  markdown_content <- str_replace_all(markdown_content, "`[^`]+`[:blank:]*", "") # remove inline code
+  markdown_content <- str_replace_all(markdown_content, "\\[([^\\]]+)\\]+\\((?:http|HTTP)[^\\)[:blank:]]+\\)", "\\1") # replace Markdown links with anchor text 
+  markdown_content <- str_replace_all(markdown_content, "!?\\[([^\\]]+)\\]+\\[[^\\][:space:]]+\\]", "\\1") # replace reference-style Markdown links with anchor text
+  # apply twice because they may be nested, e.g. [![Snapshot what I did!][1]][1]
+  markdown_content <- str_replace_all(markdown_content, "!?\\[([^\\]]+)\\]+\\[[^\\][:space:]]+\\]", "\\1") # replace reference-style Markdown links with anchor text
+  markdown_content <- str_replace_all(markdown_content, "\\[[^\\]]+\\]+:[:blank:]+(?:http|HTTP)[^\\)[:space:]]+", "") # remove Markdown link references
+  markdown_content <- str_replace_all(markdown_content, "<a[^>]+href=\"(?:http|HTTP)[^>]+>([^<]+)</a>", "\\1") # remove Markdown link references
+  markdown_content <- str_replace_all(markdown_content, "<?(?:http|HTTP)[^[:space:]]+>?[:blank:]*", "") # remove remaining links
+  markdown_content <- markdown_text(markdown_content)
+}
+
 # read text block content
 sample_400_textblocks <- fread("data/Sample400_Posts_TextBlocks.csv", header=TRUE, sep=",", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"), stringsAsFactors=FALSE)
 nrow(sample_400_textblocks)
@@ -25,16 +39,7 @@ nrow(sample_400_textblocks)
 
 # process and export text block content
 for (i in 1:nrow(sample_400_textblocks)) {
-  sample_400_textblocks[i]$TextBlockContent <- str_replace_all(sample_400_textblocks[i]$TextBlockContent, "&#xD;&#xA;", "\n") # replace escaped newlines
-  sample_400_textblocks[i]$TextBlockContent <- str_replace_all(sample_400_textblocks[i]$TextBlockContent, "\"\"", "\"") # replace escaped double quotes
-  sample_400_textblocks[i]$TextBlockContent <- str_replace_all(sample_400_textblocks[i]$TextBlockContent, "`[^`]+`[:blank:]*", "") # remove inline code
-  sample_400_textblocks[i]$TextBlockContent <- str_replace_all(sample_400_textblocks[i]$TextBlockContent, "\\[([^\\]]+)\\]+\\((?:http|HTTP)[^\\)[:blank:]]+\\)", "\\1") # replace Markdown links with anchor text 
-  sample_400_textblocks[i]$TextBlockContent <- str_replace_all(sample_400_textblocks[i]$TextBlockContent, "!?\\[([^\\]]+)\\]+\\[[^\\][:space:]]+\\]", "\\1") # replace reference-style Markdown links with anchor text
-  # apply twice because they may be nested, e.g. [![Snapshot what I did!][1]][1]
-  sample_400_textblocks[i]$TextBlockContent <- str_replace_all(sample_400_textblocks[i]$TextBlockContent, "!?\\[([^\\]]+)\\]+\\[[^\\][:space:]]+\\]", "\\1") # replace reference-style Markdown links with anchor text
-  sample_400_textblocks[i]$TextBlockContent <- str_replace_all(sample_400_textblocks[i]$TextBlockContent, "\\[[^\\]]+\\]+:[:blank:]+(?:http|HTTP)[^\\)[:space:]]+", "") # remove Markdown link references
-  sample_400_textblocks[i]$TextBlockContent <- str_replace_all(sample_400_textblocks[i]$TextBlockContent, "(?:http|HTTP)[^[:space:]]+[:blank:]*", "") # remove remaining links
-  sample_400_textblocks[i]$TextBlockContent <- markdown_text(sample_400_textblocks[i]$TextBlockContent)
+  sample_400_textblocks[i]$TextBlockContent <- remove_links_and_parse(sample_400_textblocks[i]$TextBlockContent)
 
   post_type = ""
   if (sample_400_textblocks[i]$PostId == sample_400_textblocks[i]$ParentId) { # question
@@ -55,16 +60,7 @@ nrow(sample_400_comments)
 
 # process and export text block content
 for (i in 1:nrow(sample_400_comments)) {
-  sample_400_comments[i]$CommentContent <- str_replace_all(sample_400_comments[i]$CommentContent, "&#xD;&#xA;", "\n") # replace escaped newlines
-  sample_400_comments[i]$CommentContent <- str_replace_all(sample_400_comments[i]$CommentContent, "\"\"", "\"") # replace escaped double quotes
-  sample_400_comments[i]$CommentContent <- str_replace_all(sample_400_comments[i]$CommentContent, "`[^`]+`[:blank:]*", "") # remove inline code
-  sample_400_comments[i]$CommentContent <- str_replace_all(sample_400_comments[i]$CommentContent, "\\[([^\\]]+)\\]+\\((?:http|HTTP)[^\\)[:blank:]]+\\)", "\\1") # replace Markdown links with anchor text 
-  sample_400_comments[i]$CommentContent <- str_replace_all(sample_400_comments[i]$CommentContent, "!?\\[([^\\]]+)\\]+\\[[^\\][:space:]]+\\]", "\\1") # replace reference-style Markdown links with anchor text
-  # apply twice because they may be nested, e.g. [![Snapshot what I did!][1]][1]
-  sample_400_comments[i]$CommentContent <- str_replace_all(sample_400_comments[i]$CommentContent, "!?\\[([^\\]]+)\\]+\\[[^\\][:space:]]+\\]", "\\1") # replace reference-style Markdown links with anchor text
-  sample_400_comments[i]$CommentContent <- str_replace_all(sample_400_comments[i]$CommentContent, "\\[[^\\]]+\\]+:[:blank:]+(?:http|HTTP)[^\\)[:space:]]+", "") # remove Markdown link references
-  sample_400_comments[i]$CommentContent <- str_replace_all(sample_400_comments[i]$CommentContent, "(?:http|HTTP)[^[:space:]]+[:blank:]*", "") # remove remaining links
-  sample_400_comments[i]$CommentContent <- markdown_text(sample_400_comments[i]$CommentContent)
+  sample_400_comments[i]$CommentContent <- remove_links_and_parse(sample_400_comments[i]$CommentContent)
   
   file_path <- paste0(output_dir, "/", sample_400_comments[i]$ParentId, "-", sample_400_comments[i]$PostId, "-c-", sample_400_comments[i]$CommentId, ".txt") 
   write(sample_400_comments[i]$CommentContent, file=file_path)  
